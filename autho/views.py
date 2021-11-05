b6b3a3ab8f9454dcce38b162ee24eb1ecba7ab93
kvngho <kangho@hanyang.ac.kr>
2021-11-05 18:29:13 +0900

    제발2

--- autho/views.py
+++ autho/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 # Create your views here.
 from rest_framework.response import Response
+import requests
 
-
+ID = '1013430102836002'
+SECRET = '53714de5a51b0af492466e5260c5f401'
 class TestAPIView(APIView):
     def get(self, request, *args, **kwargs):
         data = self.request.query_params
+        code = data.get('code', None)
+        # if code is None:
+        #     return Response({'error' : 'error'})
+        if data.get('code') is None:
+            data = {'code': code, 'grant_type': 'authorization_code', 'client_id':ID, 'client_secret' : SECRET,
+                    'redirect_uri' : 'https://kangho.space/autho/'}
+            res = requests.post('https://api.instagram.com/oauth/access_token', data=data)
+            return Response(res.json())
         return Response(data)
